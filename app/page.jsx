44952aa62d76bdd9e9d4e0ab27ee52360fc2297c
souqyemen <at8888w@gmail.com>
2025-12-28 20:58:47 +0300

    Delete app directory

--- app/page.jsx
+++
@@ -1,9 +0,0 @@
-import HomePage from "../components/HomePage";
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-gray-50 dark:bg-gray-900">
-      <HomePage />
-    </main>
-  );
-}
